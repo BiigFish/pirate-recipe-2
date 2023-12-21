f434a00db3baaadc7fcb67c0e4b1536a458155ec
Evan <evchan29@gmail.com>
2023-12-21 10:30:29 -0500

    404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const NotFound = () => {
   return (
     <div className="grow text-center flex flex-col justify-center items-center gap-y-4">
-      <h1 className="text-xl">Recipe not found!</h1>
+      <h1 className="text-xl">Page not found!</h1>
       <Link href="/" className="text-blue-500">
         Go Home
       </Link>
